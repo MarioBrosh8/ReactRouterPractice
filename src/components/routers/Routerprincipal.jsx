180fa2015b798ab4d4b56e1d34d53740f6a12a9b
MarioBrosh <valdezmario6@gmail.com>
2025-02-24 21:56:16 -0600

    Complete routing functionality

--- src/components/routers/Routerprincipal.jsx
+++ src/components/routers/Routerprincipal.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Inicio } from '/src/components/Inicio';
 import { Contacto } from '/src/components/Contacto';
 import { Articulos } from '/src/components/Articulos';
@@ -6,14 +6,33 @@ import { Articulos } from '/src/components/Articulos';
 export const Routerprincipal = () => {
     return (
         <BrowserRouter>
-            <Routes>
+            <h1>Header</h1>
+            <hr />
 
-                <Route path="/" element={<Inicio />} />
-                <Route path="/Inicio" element={<Inicio />} />
-                <Route path="/Contacto" element={<Contacto />} />
-                <Route path="/Articulos" element={<Articulos />} />
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/Inicio" className={({isActive}) => isActive ? "active" : ""}>Inicio</NavLink>
+                        <NavLink to="/Contacto" className={({isActive}) => isActive ? "active" : ""}>Contacto</NavLink>
+                        <NavLink to="/Articulos" className={({isActive}) => isActive ? "active" : ""}>Articulos</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-            </Routes>
+
+            <h2>Menu</h2>
+            <hr />
+
+            <section>
+                <Routes>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/Inicio" element={<Inicio />} />
+                    <Route path="/Contacto" element={<Contacto />} />
+                    <Route path="/Articulos" element={<Articulos />} />
+                    <Route path="*" element={<h1>Error 404</h1>} />
+                </Routes>
+            </section>
+
         </BrowserRouter>
     )
 }
